07fb4a0b56f595e29ffecdb453cc2f0e09631779
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-03-29 16:29:26 +0200

    Add appointment and treatment component

--- src/app/hairdresser/hairdresser.module.ts
+++ src/app/hairdresser/hairdresser.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HairdresserComponent } from './hairdresser.component';
+import { TreatmentComponent } from '../treatment/treatment.component';
+import { AppointmentComponent } from '../appointment/appointment.component';
 
 
 
 @NgModule({
-  declarations: [HairdresserComponent],
+  declarations: [HairdresserComponent, TreatmentComponent, AppointmentComponent],
   imports: [
     CommonModule
   ]
