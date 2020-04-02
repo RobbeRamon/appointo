2e60cf8dba6612be9e72beb3e96e5039874bbc23
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-04-02 17:31:01 +0200

    Add main navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { AppComponent } from "./app.component";
+import { MainNavComponent } from "./main-nav/main-nav.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainNavComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainNavComponent],
+  imports: [BrowserModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
