383250502df17f96ff5d528d1eb3d87bccb0206e
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-04-03 15:44:17 +0200

    Add route in button of hairdresser in hairresserlist

--- src/app/hairdresser.model.ts
+++ src/app/hairdresser.model.ts
@@ -12,6 +12,10 @@ export class Hairdresser {
     return this._name;
   }
 
+  get id() {
+    return this._id;
+  }
+
   static fromJSON(json: HairdresserJson): Hairdresser {
     const hairdresser = new Hairdresser(json.name);
     hairdresser._id = json.id;
