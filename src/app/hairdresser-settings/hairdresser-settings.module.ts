347e13c65e30262bf2e7a623931b26d7f83834f8
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-05-08 14:11:43 +0200

    Add frontend for managing openinghours

--- src/app/hairdresser-settings/hairdresser-settings.module.ts
+++ src/app/hairdresser-settings/hairdresser-settings.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { ManageWorkdaysComponent } from "./manage-workdays/manage-workdays.component";
 import { SettingsOverviewComponent } from "./settings-overview/settings-overview.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MdbootstrapModule } from "../mdbootstrap/mdbootstrap.module";
 
 const routes: Routes = [
   { path: "manage/settings", component: SettingsOverviewComponent },
@@ -10,6 +12,11 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ManageWorkdaysComponent, SettingsOverviewComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    MdbootstrapModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+  ],
 })
 export class HairdresserSettingsModule {}
--- src/app/hairdresser-settings/manage-workdays/manage-workdays.component.ts
+++ src/app/hairdresser-settings/manage-workdays/manage-workdays.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
+
+function validateTimeBlock(control: FormGroup): { [key: string]: any } {
+  if (
+    new Date(control.get("start").value).getTime() >=
+    new Date(control.get("end").value).getTime()
+  ) {
+    return { hoursNotCorrect: true };
+  }
+  return null;
+}
 
 @Component({
-  selector: 'app-manage-workdays',
-  templateUrl: './manage-workdays.component.html',
-  styleUrls: ['./manage-workdays.component.scss']
+  selector: "app-manage-workdays",
+  templateUrl: "./manage-workdays.component.html",
+  styleUrls: ["./manage-workdays.component.scss"],
 })
 export class ManageWorkdaysComponent implements OnInit {
+  public days: FormGroup;
+  public daysArrayNl = [
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag",
+    "Zondag",
+  ];
+  public daysArrayEn = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.days = this.fb.group({
+      monday: this.fb.array([]),
+      tuesday: this.fb.array([]),
+      wednesday: this.fb.array([]),
+      thursday: this.fb.array([]),
+      friday: this.fb.array([]),
+      saturday: this.fb.array([]),
+      sunday: this.fb.array([]),
+    });
+    // this.getHours(0).valueChanges.subscribe((hours) => {
+    //   const lastElement = hours[hours.length - 1];
+    //   if (lastElement.start && lastElement.end) {
+    //     this.getHours(0).push(this.createTimeBlock());
+    //   }
+    // });
   }
 
+  getHours(day: number): FormArray {
+    return <FormArray>this.days.get(this.daysArrayEn[day]);
+  }
+
+  createTimeBlock(): FormGroup {
+    return this.fb.group(
+      {
+        start: [""],
+        end: [""],
+      },
+      { validator: validateTimeBlock }
+    );
+  }
+
+  addWorkBlock(day: number) {
+    this.getHours(day).push(this.createTimeBlock());
+  }
+
+  removeWorkBlock(day: number, block: number) {
+    this.getHours(day).removeAt(block);
+  }
 }
