15da8999327b2665e36c95730e710fa587c8ec24
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-05-13 17:28:57 +0200

    Add first e2e test

---
+++ cypress/integration/hairdresserlist.spec.js
@@ -0,0 +1,34 @@
+describe('Hairdresser list tests', function () {
+    it('delayed respsone brings state out of sync', () => {
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: '/api/hairdressers',
+            status: 200,
+            response: 'fixture:hairdressers.json',
+        });
+        cy.route({
+            delay: 2000,
+            method: 'GET',
+            url: '/api/hairdressers/?name=al',
+            status: 200,
+            respone: 'fixture:alexa.json',
+        }).as('getALhairdresser');
+        cy.route({
+            delay: 2000,
+            method: 'GET',
+            url: '/api/hairdressers/?name=ha',
+            status: 200,
+            respone: 'fixture:hairloungeMarlies.json',
+        }).as('getHMhairdresser');
+
+
+        cy.visit('/hairdresser/list');
+        cy.get('[data-cy=filterInput]').type('al');
+        cy.wait(300);
+        cy.get('[data-cy=filterInput]').type('{backspace}{backspace}ha');
+        cy.wait(['@getALhairdresser', '@getHMhairdresser']);
+        cy.get('[data-cy=hairdresserCard]').should('have.length', 1);
+        cy.get('[data-cy=hairdresser-title]').should('contain', 'Hairlounge Marlies');
+    })
+});
