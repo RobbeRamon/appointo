c34d518b7ecdcb41e251231c2d014ff08b9d91c7
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-05-16 12:52:17 +0200

    Add landing components

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MdbootstrapModule } from "../mdbootstrap/mdbootstrap.module";
+import { BenefitsComponent } from "./benefits/benefits.component";
+import { LandingComponent } from "./landing/landing.component";
+import { HairdresserModule } from "../hairdresser/hairdresser.module";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MdbootstrapModule, ReactiveFormsModule],
-  exports: []
+  declarations: [BenefitsComponent, LandingComponent],
+  imports: [
+    CommonModule,
+    MdbootstrapModule,
+    ReactiveFormsModule,
+    HairdresserModule,
+  ],
+  exports: [],
 })
 export class LandingModule {}
